10d9b232b8308de3467b31569abfc16ba97d2e95
itm154 <ashrulfahmi@gmail.com>
2023-12-30 13:14:59 +0800

    Revert "Rose pine"
    This reverts commit db2e4ed82608cc4bd74b305dd057361cea8aea20.

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,6 +1,4 @@
 local wezterm = require("wezterm")
-local colors = require("lua/rose-pine").colors()
-local window_frame = require("lua/rose-pine").window_frame()
 
 local config = {}
 
@@ -8,15 +6,45 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16
 config.warn_about_missing_glyphs = false
 
 config.tab_bar_at_bottom = true
-config.window_frame = window_frame
+config.window_frame = {
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 12,
+	active_titlebar_bg = "#181825",
+	inactive_titlebar_bg = "#181825",
+}
 
 config.hide_tab_bar_if_only_one_tab = true
-config.colors = colors
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			fg_color = "#CDD6F4",
+			bg_color = "#313244",
+		},
+		inactive_tab = {
+			fg_color = "#A6ADC8",
+			bg_color = "#1E1E2E",
+		},
+		inactive_tab_hover = {
+			fg_color = "#CDD6F4",
+			bg_color = "#45475A",
+		},
+		new_tab = {
+			fg_color = "#A6ADC8",
+			bg_color = "#181825",
+		},
+		new_tab_hover = {
+			fg_color = "#A6ADC8",
+			bg_color = "#45475A",
+		},
+		inactive_tab_edge = "#1E1E2E",
+	},
+}
 
 config.window_padding = {
 	left = 2,
